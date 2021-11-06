fa56e07feed298b516c759ca5a56c3c85361d4f0
Shaun Loo <shaunloo10@gmail.com>
2021-11-05 21:18:05 -0500

    Initial macOS scripts. SSH untested
    	new file:   .gitignore
    	new file:   boot-nogui.sh
    	new file:   boot.sh

---
+++ boot-nogui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+vm_mem=2560
+img_hdd0="Debian.qcow2"
+
+qemu-system-x86_64 -machine q35,accel=hvf \
+-smp 4,cores=2 -m $vm_mem -hda $img_hdd0 \
+-device e1000,netdev=net0 \
+-netdev user,id=net0,hostfwd=tcp::5555-:22 \
+-nographic
